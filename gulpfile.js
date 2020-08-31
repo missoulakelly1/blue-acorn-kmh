dcc2e5500b5636b86b1d9ea1e05a61b33aaf1ba1
missoulakelly1 <kelly.hanson@swiftcurrentweb.com>
2020-08-31 13:37:09 -0400

    adding project files for blue acorn demo

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var replace = require('gulp-replace');
+var autoprefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
+
+gulp.task('sass', function() {
+    return gulp.src('./sass/styles.scss')
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(rename('styles.scss.liquid'))
+        .pipe(replace('"{{', '{{'))
+        .pipe(replace('}}"', '}}'))
+        .pipe(gulp.dest('./assets/'));
+});
+
+gulp.task('default', function() {
+    
+    gulp.watch('./sass/**/*.scss', gulp.series('sass'));
+});
